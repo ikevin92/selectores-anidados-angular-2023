53592108e0a07813a427fbd96bd709deae0a7813
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-25 00:53:02 -0500

    selector de regiones

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Region } from '../interfaces/country.interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountriesService {
+  private _regions: Region[] = [
+    Region.Africa,
+    Region.Europe,
+    Region.Asia,
+    Region.Oceania,
+    Region.Americas,
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  get regions(): Region[] {
+    return [...this._regions];
+  }
 }
