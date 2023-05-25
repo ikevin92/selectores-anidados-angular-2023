24e30449e4556c91c18caa78d32323ae33e0ac01
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-25 01:22:10 -0500

    convertir los paises y mostrarlos en pantalla

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,26 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
-import { Region } from '../../interfaces/country.interfaces';
+import { Region, SmallCountry } from '../../interfaces/country.interfaces';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
   styles: [],
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
     borders: ['', Validators.required],
   });
 
+  public countriesByRegion: SmallCountry[] = [];
+
   constructor(
     private fb: FormBuilder,
     private countriesService: CountriesService
   ) {}
 
+  ngOnInit(): void {
+    this.onRegionChanged();
+  }
+
   get regions(): Region[] {
     return this.countriesService.regions;
+  }
+
+  onRegionChanged(): void {
+    this.myForm
+      .get('region')!
+      .valueChanges.pipe(
+        switchMap((region) =>
+          this.countriesService.getCountriesByRegion(region)
+        )
+      )
+      .subscribe((countries) => (this.countriesByRegion = countries));
   }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Region } from '../interfaces/country.interfaces';
+import {
+  Country,
+  Region,
+  SmallCountry,
+} from '../interfaces/country.interfaces';
+import { Observable, map, of, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountriesService {
+  private baseUrl: string = 'https://restcountries.com/v3.1';
   private _regions: Region[] = [
     Region.Africa,
     Region.Europe,
@@ -13,9 +20,29 @@ export class CountriesService {
     Region.Americas,
   ];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get regions(): Region[] {
     return [...this._regions];
+  }
+
+  getCountriesByRegion(region: Region): Observable<SmallCountry[]> {
+    if (!region) return of([]);
+
+    const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+
+    return this.http.get<Country[]>(url).pipe(
+      map(
+        (
+          countries // con el map convertimos la data
+        ) =>
+          countries.map((country) => ({
+            name: country.name.common,
+            cca3: country.cca3,
+            borders: country.borders ?? [],
+          }))
+      )
+      // tap((response) => console.log({ response }))
+    );
   }
 }
